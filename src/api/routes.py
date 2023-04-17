71c3d856952f3abcf2407eba463568d7a6e5be06
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-04-17 09:59:21 +0000

    initial endpoints (without implementation)

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,17 +8,21 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 @api.route('/user', methods=['POST'])
-def add_one_user():
+def add_user():
+    # implementation
     pass
 
 @api.route('/horse', methods=['GET'])
 def get_all_horses():
+    # implementation
     pass
 
 @api.route('/horse', methods=['POST'])
-def add_one_horse():
+def add_horse():
+    # implementation
     pass
 
 @api.route('/horse/<int:id>', methods=['GET'])
-def get_horse():
+def get_horse(id):
+    # implementation
     pass
